df000d8ccf8fa8fc80d23245cf27bf6f7de65eb2
naru-rama <mamoru.recruit@gmail.com>
2023-04-25 00:19:40 +0900

    Initial Commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 中原護 on 2023/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
