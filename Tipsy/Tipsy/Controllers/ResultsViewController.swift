3c1b7455c287728cca9ff84c19f57ec3f295c626
naru-rama <mamoru.recruit@gmail.com>
2023-05-01 13:06:51 +0900

    レッスン139完了

---
+++ Tipsy/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by 中原護 on 2023/05/01.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
