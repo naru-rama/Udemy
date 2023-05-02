8951f6aa7f808b4c39a5f70608c8bfb95e49f05c
naru-rama <mamoru.recruit@gmail.com>
2023-05-03 02:11:58 +0900

    レッスン167完了

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -16,7 +16,21 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                let jsonData = String(data: data!, encoding: .utf8)
+                print(jsonData)
+            }
+            task.resume()
+        }
     }
     
 }
