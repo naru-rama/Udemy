aa2ef1ab50757e356b5e045f136da8dc0a577784
naru-rama <mamoru.recruit@gmail.com>
2023-05-13 01:25:29 +0900

    構文の修正

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -15,32 +15,26 @@ protocol CoinManagerDelegate {
 
 struct CoinManager {
     var delegate: CoinManagerDelegate?
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "0AF7E62A-40B6-49DA-985B-99E5D77B55A3"
+    let baseURL = ""
+    let apiKey = ""
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    self.delegate?.didFailWithError(error: error!)
-                    return
-                }
                 
-                if let safeData = data {
-                    if let price = parseJSON(safeData) {
-                        let priceString = String(format: "%.2f", price)
-                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
-                    }
-                    
-                }
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                self.delegate?.didFailWithError(error: error)
+                return
             }
-            task.resume()
+            guard let safeData = data, let price = parseJSON(safeData) else { return }
+            let priceString = String(format: "%.2f", price)
+            self.delegate?.didUpdatePrice(price: priceString, currency: currency)
         }
+        task.resume()
     }
     
     func parseJSON(_ data: Data) -> Double? {
