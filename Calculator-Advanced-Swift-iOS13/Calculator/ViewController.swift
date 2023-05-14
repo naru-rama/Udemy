c2aaa584410e0e4353a5bb0308a3bec0397785ec
naru-rama <mamoru.recruit@gmail.com>
2023-05-14 14:41:04 +0900

    レッスン307完了

--- Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
@@ -12,19 +12,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
         
-        //What should happen when a non-number button is pressed
-    
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+        guard let calcMethod = sender.currentTitle else { return }
+        if calcMethod == "+/-" {
+            displayLabel.text = String(number * -1)
+        } else if calcMethod == "AC" {
+            displayLabel.text = "0"
+        } else if calcMethod == "%" {
+            displayLabel.text = String(number * 0.01)
+        }
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        guard let numValue = sender.currentTitle else { return }
+        if isFinishedTypingNumber {
+            displayLabel.text = numValue
+            isFinishedTypingNumber = false
+        } else {
+            if numValue == "." {
+                guard let currentDisplayValue = Double(displayLabel.text!) else {
+                    fatalError("Cannot convert display label text to a Double!")
+                }
+                
+                let isInt = floor(currentDisplayValue) == currentDisplayValue
+                if !isInt {
+                    return 
+                }
+            }
+            displayLabel.text = displayLabel.text! + numValue
+        }
         
-        //What should happen when a number is entered into the keypad
-    
     }
 
 }
